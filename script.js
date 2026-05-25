c9ef3a08d3e575b5b7f2eb2cd22c105e38444267
Raul <raulfabiansaenzmaguina@gmail.com>
2026-05-25 15:14:53 -0500

    Actualicé diseño y agregué imágenes en sección nosotros

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("Página cargada");
+function mostrarImagen(numero){
+
+    const imagenes =
+        document.querySelectorAll(".imagen-opcion");
+
+    const items =
+        document.querySelectorAll(".item");
+
+    imagenes.forEach(img=>{
+        img.classList.remove("activa");
+    });
+
+    items.forEach(item=>{
+        item.classList.remove("active");
+    });
+
+    document
+        .getElementById("img" + numero)
+        .classList.add("activa");
+
+    items[numero - 1]
+        .classList.add("active");
+}
